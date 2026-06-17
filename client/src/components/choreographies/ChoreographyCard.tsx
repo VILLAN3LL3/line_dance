34d09edba832c115930de8bb24750afac7c6c684
VILLAN3LL3 <development@gutfrau.de>
2026-06-17 20:58:19 +0200

    fix tag and restart information on small cards

--- client/src/components/choreographies/ChoreographyCard.tsx
+++ client/src/components/choreographies/ChoreographyCard.tsx
@@ -6,7 +6,15 @@ import { deleteChoreographyRating, setChoreographyRating } from "../../api";
 import { Choreography } from "../../types";
 import { buildChoreographyClipboardText } from "../../utils/choreographyClipboard";
 import { getYouTubeVideoEmbedUrl } from "../../utils/youtube";
-import { ActionButton, Card, ExternalLink, LevelBatch, Tag, TagGroup, YouTubeVideo } from "../shared/ui";
+import {
+  ActionButton,
+  Card,
+  ExternalLink,
+  LevelBatch,
+  Tag,
+  TagGroup,
+  YouTubeVideo,
+} from "../shared/ui";
 import { SongSearchButtons } from "./SongSearchButtons";
 import { StarRating } from "./StarRating";
 
