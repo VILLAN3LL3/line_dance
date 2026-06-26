0f279e4994bae5436c9b65ae62d7c5475681035f
VILLAN3LL3 <development@gutfrau.de>
2026-06-26 14:45:12 +0200

    open search in separate tab

--- client/src/components/courses/CourseDetailSessionsSection.tsx
+++ client/src/components/courses/CourseDetailSessionsSection.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 import { getSessionBadgeLabel, getSessionBadgeStatus } from "../../utils/courseStatus";
-import {
-  ActionButton,
-  ActionGroup,
-  CheckboxFilter,
-  EmptyState,
-  LoadingState,
-  Section,
-  StatusBadge,
-} from "../shared/ui";
+import { ActionButton, ActionGroup, CheckboxFilter, EmptyState, LoadingState, Section, StatusBadge } from "../shared/ui";
 
 import type { Session, StepFigureSuggestion } from "../../types";
 
