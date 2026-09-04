ce56c9e52b9f82ab7a61dabc7bac2750264fc3cc
VILLAN3LL3 <development@gutfrau.de>
2026-09-04 19:43:22 +0200

    courseid and semester optional

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,22 +1,9 @@
 import axios from "axios";
 
 import {
-  Choreography,
-  ChoreographyFormData,
-  DanceCourse,
-  DanceGroup,
-  DuplicateChoreography,
-  LearnedChoreography,
-  LevelOption,
-  PaginatedResponse,
-  SavedFilterConfiguration,
-  SearchFilters,
-  Session,
-  SessionChoreography,
-  SessionStepFigureSuggestionsResult,
-  StepFigureDefinition,
-  StepFigureOption,
-  StepFigureSuggestion,
+  Choreography, ChoreographyFormData, DanceCourse, DanceGroup, DuplicateChoreography, LearnedChoreography, LevelOption, PaginatedResponse,
+  SavedFilterConfiguration, SearchFilters, Session, SessionChoreography, SessionStepFigureSuggestionsResult, StepFigureDefinition,
+  StepFigureOption, StepFigureSuggestion
 } from "./types";
 
 import type { Trainer } from "./types";
